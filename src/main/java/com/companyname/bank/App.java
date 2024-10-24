491e825ab746e2a979cd11a6c2200391a6e8ac89
Karthik Dasari <73741537+Karthik-vkpc@users.noreply.github.com>
2024-10-24 12:34:31 +0530

    new Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new changes on git repo");
+        System.out.println( "another change on git repo");
     }
 }
