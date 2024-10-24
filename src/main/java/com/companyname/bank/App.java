079a3b581a1b6ae45458740e6a746a192e9bacd4
Karthik Dasari <73741537+Karthik-vkpc@users.noreply.github.com>
2024-10-24 12:30:16 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "test code change for github webhook");
+        System.out.println( "new changes on git repo");
     }
 }
